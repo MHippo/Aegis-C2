30ec738267756262ffda62f81b5020fbca12cc09
MHippo <DONOTUSE@email.com>
2026-03-10 17:28:26 -0400

    changing the name

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,20 @@
-use actix_web::{web, App, HttpServer, middleware};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use sea_orm::{Database, DbConn};
 
-fn main(){
-	
+
+#[get("/")]
+async fn index() -> impl Responder {
+	HttpResponse::Ok().body("Hello world!")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+		.service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
