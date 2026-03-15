d5672433a69d8a7ee40fba0cd75ad4a984546d09
MHippo <DONOTUSE@email.com>
2026-03-15 08:47:12 -0400

    add some egui boilerplate and fix names being aegis-shell instead of argus-shell

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,30 @@
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([1220.0, 720.0]),
+        ..Default::default()
+    };
+    let _ = eframe::run_native(
+    	"My egui App",
+     	options, 
+     	Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc))))
+     );
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+       egui::CentralPanel::default().show(ctx, |ui| {
+           ui.heading("Hello World!");
+       });
+   }
 }
